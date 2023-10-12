497a449fea64ae425ff8c7650b89b0be2328e5f0
skyloft7 <skyloftmb@gmail.com>
2023-10-12 09:05:52 +0530

    forgot to commit EditorDocument

---
+++ src/custom/EditorDocument.java
@@ -0,0 +1,21 @@
+package custom;
+
+import javax.swing.text.DefaultStyledDocument;
+
+public class EditorDocument extends DefaultStyledDocument {
+
+    public EditorDocument(){
+        super();
+    }
+
+    public void lockWriter(){
+        writeLock();
+    }
+    public void unlockWriter(){
+        writeUnlock();
+    }
+
+    public boolean hasWriteLock(){
+        return getCurrentWriter() != null;
+    }
+}
