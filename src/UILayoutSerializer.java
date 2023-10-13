0272549b8a5f8cff42132ce92ebaf6d35171bd68
skyloft7 <skyloftmb@gmail.com>
2023-10-13 12:56:40 +0530

    moved ui saving logic from Main

---
+++ src/UILayoutSerializer.java
@@ -0,0 +1,107 @@
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+import custom.JDockableWindow;
+import custom.JDockspace;
+import org.simpleyaml.configuration.ConfigurationSection;
+import org.simpleyaml.configuration.file.YamlFile;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+
+public class UILayoutSerializer {
+
+    public void save(JDockspace jDockspace){
+        //TODO: Move this to EDT
+
+
+        try {
+            YamlFile preferencesFile = new YamlFile("Settings.yaml");
+            preferencesFile.createNewFile(true);
+
+
+
+            preferencesFile.set("DarkMode", FlatLaf.isLafDark());
+            ConfigurationSection dockspacePrefs = preferencesFile.createSection("Dockspace");
+
+            for(JDockableWindow jDockableWindow : jDockspace.getWindows().values()){
+                ConfigurationSection windowPrefs = dockspacePrefs.createSection(jDockableWindow.getTitle());
+                windowPrefs.set("DockPosition", jDockableWindow.getDockPos());
+                windowPrefs.set("Width", jDockableWindow.getSize().width);
+                windowPrefs.set("Height", jDockableWindow.getSize().height);
+
+            }
+
+
+
+
+            preferencesFile.save();
+        }
+        catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public void restore(JDockspace jDockspace){
+        YamlFile preferencesFile = new YamlFile("Settings.yaml");
+
+        if(!preferencesFile.exists()) return;
+
+
+        try {
+            preferencesFile.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        if(preferencesFile.exists()){
+
+
+            boolean darkMode = preferencesFile.getBoolean("DarkMode");
+            {
+
+                if (darkMode) {
+                    System.out.println("Dark mode is best mode");
+                    FlatDarkLaf.setup();
+                } else {
+                    FlatLightLaf.setup();
+                }
+
+                Window window = SwingUtilities.getWindowAncestor(jDockspace);
+                SwingUtilities.updateComponentTreeUI(window);
+                window.repaint();
+            }
+
+            ConfigurationSection dockspace = preferencesFile.getConfigurationSection("Dockspace");
+            for(JDockableWindow jDockableWindow : jDockspace.getWindows().values()){
+                ConfigurationSection c = dockspace.getConfigurationSection(jDockableWindow.getTitle());
+
+                System.out.println(c.getName());
+
+                Object dockPos = c.get("DockPosition");
+                jDockableWindow.setPreferredSize(new Dimension(c.getInt("Width"), c.getInt("Height")));
+
+                jDockspace.remove(jDockableWindow);
+                jDockspace.add(jDockableWindow, dockPos);
+                jDockspace.revalidate();
+                jDockspace.repaint();
+
+                jDockableWindow.setDockPos(dockPos);
+
+
+
+
+            }
+
+
+
+
+        }
+    }
+
+
+
+
+
+}
