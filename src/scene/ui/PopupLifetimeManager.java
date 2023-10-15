824b3e8dcdcb9dd65224f2bcc438e50cba10f65d
skyloft7 <skyloftmb@gmail.com>
2023-10-15 13:09:35 +0530

    moved popup logic to reusable class

---
+++ src/scene/ui/PopupLifetimeManager.java
@@ -0,0 +1,31 @@
+package scene.ui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.util.HashMap;
+
+public class PopupLifetimeManager {
+    private HashMap<String, PopupContext> popups = new HashMap<>();
+
+    public void install(JComponent j){
+        j.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                for(PopupContext c : popups.values()){
+                    Point p = SwingUtilities.convertPoint(j, e.getPoint(), c.component);
+
+                    if(!c.component.contains(p)){
+                        if(c.popupListener != null) c.popupListener.popupClosing(c.popup);
+                        c.popup.hide();
+                    }
+                }
+            }
+        });
+    }
+
+    public void register(String id, PopupContext popupContext) {
+        popups.put(id, popupContext);
+    }
+}
